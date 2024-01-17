1c2b52a942de0d9e2c03a3a6572955ce3171ad16
SXFreell <xfreell@163.com>
2024-01-18 03:59:20 +0800

    feat: init

---
+++ vite.background.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { CRX_BACKGROUND_OUTDIR } from './global.config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: CRX_BACKGROUND_OUTDIR,
+    lib: {
+        entry: [path.resolve(__dirname, 'src/background/index.tsx')],
+        formats: ['cjs'],
+        fileName: () => {
+            return 'background.js'
+        }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
