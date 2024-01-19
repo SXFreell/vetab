ca77397e72710d166f5b268ab9ae0cf3d5198405
SXFreell <xfreell@163.com>
2024-01-20 02:32:48 +0800

    feat: 增加固定按钮和实验性按钮

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import "./index.less"
 import React, { useEffect } from 'react'
-import {IconClose, IconPlus} from '@arco-design/web-react/icon'
+import {IconClose, IconPlus, IconExperiment, IconPushpin, IconLeft} from '@arco-design/web-react/icon'
 
 // 添加vetab
 const htmlElement = document.querySelector("html")
@@ -11,6 +11,8 @@ htmlElement?.insertBefore(vetab, htmlElement.firstChild)
 
 function VeTab() {
     const [tabList, setTabList] = React.useState<any[]>([])
+    const [pin, setPin] = React.useState<boolean>(false)
+    const [fixed, setFixed] = React.useState<boolean>(false)
 
     chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
         if (message.type === 'activeTab') {
@@ -22,13 +24,41 @@ function VeTab() {
         chrome.runtime.sendMessage({ type: 'getTabs' })
     }, [])
 
+    useEffect(() => {
+        if (pin) {
+            vetab.classList.add('vetab-active')
+            document.body.style.marginLeft = '200px'
+            document.body.style.width = 'calc(100% - 200px)'
+        } else {
+            vetab.classList.remove('vetab-active')
+            document.body.style.marginLeft = '50px'
+            document.body.style.width = 'calc(100% - 50px)'
+        }
+        if (fixed) {
+            document.body.classList.add('vetab-fixed')
+        } else {
+            document.body.classList.remove('vetab-fixed')
+        }
+    })
+
     return (
         <>
             <div className='vetab-header'>
                 <div className='vetab-header-icon'>
-                    
+                    <IconExperiment style={{color: '#CCC', fontSize: 20, strokeWidth: 3}}
+                        onClick={() => { setFixed(!fixed) }}
+                    />
                 </div>
-                <div className='vetab-header-title'></div>
+                <div className='vetab-header-after'>
+                    {
+                        pin ? <IconLeft style={{color: '#CCC', fontSize: 20, strokeWidth: 3}}
+                                onClick={() => { setPin(!pin) }}
+                            />
+                            : <IconPushpin style={{color: '#CCC', fontSize: 20, strokeWidth: 3}}
+                                onClick={() => { setPin(!pin) }}
+                            />
+                    }
+                </div>
             </div>
             <div className='vetab-content'>
                 {
